c1fd171bc057b6b9e913c886e2a3b5b9c7b37b64
lucael <lucael@naver.com>
2017-06-28 15:43:27 +0900

    주문조회 페이지 css 교정

--- coto/m/check_order.php
+++ coto/m/check_order.php
@@ -31,13 +31,36 @@ if( ($name && $od_id && $hp1 && $hp2 && $hp3) || $member[mb_id] ) {
 					<div class="productName line1row taCenter"><b><a <?="href=\"".G5_SHOP_URL."/grouppurchase.php?gpcode=".$row[gpcode]."&gp_id=".$row[it_id]."&ca_id=".$row[ca_id]."\" class=\"sct_a sct_img\""?>><?=stripslashes($row[it_name])?></a></b></div>
 					<div class="productImg"><?=$image?></div>
 					<div class="productInfo">
-						<div class="gpcode"><label>공동구매명</label><p><?=$공구명?></p></div>
-						<div class="od_id"><label>주문번호</label><p><?=$row[od_id]?></p></div>
-						<div class="ymd"><label>주문일시</label><p><?=date("Y.m.d H:m:s",strtotime($row[od_date]))?></p></div>
-						<div class="qty"><label>수량</label><p><?=$row[it_qty]?></p></div>
-						<div class="price"><label>단가</label><p><?=number_format($row[it_org_price])?></p></div>
-						<div class="sumPrice"><label>합계</label><p><?=number_format($row[total_orgprice])?></p></div>
-						<div class="stats"><label>주문상태</label><p><?=$v_stats[$row[stats]]?></p></div>
+						<table width="100%" cellpadding="0" cellspacing="0" border="0">
+							<tr>
+								<th width="60">공동구매</th>
+								<td class="txtwrap"><?=$공구명?></td>
+							</tr>
+							<tr>
+								<th>주문번호</th>
+								<td class="txtwrap"><?=$row[od_id]?></td>
+							</tr>
+							<tr>
+								<th>주문일시</th>
+								<td class="txtwrap"><?=date("Y.m.d H:m:s",strtotime($row[od_date]))?></td>
+							</tr>
+							<tr>
+								<th>수량</th>
+								<td class="txtwrap"><?=$row[it_qty]?></td>
+							</tr>
+							<tr>
+								<th>단가</th>
+								<td class="txtwrap"><?=number_format($row[it_org_price])?></td>
+							</tr>
+							<tr>
+								<th>합계</th>
+								<td class="txtwrap"><?=number_format($row[total_orgprice])?></td>
+							</tr>
+							<tr>
+								<th>주문상태</th>
+								<td class="txtwrap"><?=$v_stats[$row[stats]]?></td>
+							</tr>
+						</table>
 					</div>
 				</li>
 <?
@@ -47,3 +70,25 @@ if( ($name && $od_id && $hp1 && $hp2 && $hp3) || $member[mb_id] ) {
 <?
 }
 ?>
+
+<style>
+	.productInfo div label { float:left; }
+	.productInfo .txtwrap {
+	text-align: right;
+	width: 124px;
+	height: 14px;
+	margin: 4px 0px 4px 0px;
+	display: inline-block;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: normal;
+	line-height: 1.2;
+	/*text-align: left;*/
+	word-wrap: break-word;
+	display: -webkit-box;
+	-webkit-line-clamp: 3;
+	-webkit-box-orient: vertical;
+}
+
+</style>
