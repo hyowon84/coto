3598565b57980c2f92c7a78918dd75fe788e3854
lucael <lucael@naver.com>
2017-07-25 12:10:51 +0900

    경매관련 소스 수정

--- lib/shop.lib.php
+++ lib/shop.lib.php
@@ -593,7 +593,6 @@ class auction_list
 				$sql_order = " order by {$this->order_by} ";
 			}
 
-			$sql_auction_item = str_replace('#상품기본조건#', " AND ac_yn = 'Y'  ", $sql_auction_item);
 			$it_result = sql_query($sql_auction_item);
 			$sql_limit = " limit " . $this->from_record . " , " . ($this->list_mod * $this->list_row);
 			
